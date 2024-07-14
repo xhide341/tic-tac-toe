1f0c6f049276d10241c62180ef925fa553204334
xhide341 <shawnehgn10@gmail.com>
2024-07-14 20:00:24 +0800

    add progress to script

---
+++ script.js
@@ -0,0 +1,77 @@
+// tic tac toe game
+const gameBoard = function () {
+
+    let board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]   
+    ];
+
+    // player 1 is x
+    let player1 = 0;
+    // player 2 is o
+    let player2 = 1;
+
+    function checkWinner(board) {
+        // check rows
+        for (let i = 0; i < 3; i++) {
+            if (board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
+                return board[i][0];
+            }
+        }
+
+        // check columns
+        for (let i = 0; i < 3; i++) {
+            if (board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
+                return board[0][i];
+            }
+        }
+        // check diagonals
+        if (board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+            return board[0][0];
+        } else if (board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+            return board[0][2];
+        }
+
+        // return 0 if no winner
+        return 0;
+    }
+
+    function checkDraw() {
+        let draw = true;
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                if (board[i][j] === 0) {
+                    draw = false;
+                }
+            }
+        }
+        return draw;
+    }
+    function resetBoard() {
+        board = [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0]
+        ];
+    }
+
+    return {
+        board: board,
+        makeMove: function (x, y) {
+            if (board[x][y] === 0) {
+                board[x][y] = 1;
+            } else {
+                board[x][y] = 0;
+            }
+        },
+        reset: function () {
+            resetBoard();
+        },
+        
+    }
+
+
+}
+
+
